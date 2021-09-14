8956438813258ea0c6f488daf0617598012b8a56
jacobbethany <jabwareman@gmail.com>
2021-09-14 00:09:05 -0400

    Update UnitTest.java
    Resubmitted with tabs stripped. Thanks, again, VS Code.

--- UnitTest.java
+++ UnitTest.java
@@ -15,27 +15,27 @@ class CustomException extends Exception {
 //Make a class with static methods to handle unit testing.
 class FakeUnitTest {
   FakeUnitTest (  ) {
-		}
+  }
 
   //This will return a string to prepend to the assertion error displayed in assertEquals upon failure.
   private static String  getCurrentLineNumber (  ) {
     //return "Line #" + Thread .currentThread (  ) .getStackTrace (  ) [ 1 ] .getLineNumber (  ) + ": "; //This method.
     //return "Line #" + Thread .currentThread (  ) .getStackTrace (  ) [ 2 ] .getLineNumber (  ) + ": "; //assertEquals method.
     return "Line #" + Thread .currentThread (  ) .getStackTrace (  ) [ 3 ] .getLineNumber (  ) + ": "; //The function that called the assertEquals method.
-		}
+  }
   public static void  assertEquals ( Object a, Object b ) throws Exception {
     if ( a == null) {
       throw new Exception ( getCurrentLineNumber (  ) + "Error: assertEquals called upon a null a term.\n" );
-				}
+    }
 
     if (  b == null ) {
       throw new Exception ( getCurrentLineNumber (  ) + "Error: assertEquals called upon a null a term.\n" );
-				}
+    }
 
     if ( ! a .equals ( b ) ) {
       throw new Exception ( getCurrentLineNumber (  ) + "Error: Objects are not equal; the assertion has failed.\n" );
-				}
-		}
+    }
+  }
 } //end of FakeUnitTest class.
 
 //Define a class that manages a set (unordered array filled with unique entities)
@@ -50,18 +50,18 @@ class CustomSet {
   CustomSet (  ) {
     m_objects = new Object [ 10 ];
     m_i_next_index = 0;
-		}
+  }
 
   //Inform the caller if the set is empty.
   Boolean isEmpty (  ) {
     return m_i_next_index == 0;
-	 }
+  }
 
  //Add a node to the set, expanding the array, if necessary.
  Boolean addNode ( Object obj ) {
    if ( containsNode ( obj ) ) {
      return false;
-			}
+   }
 
    if ( m_i_next_index == m_objects .length ) {
 
@@ -81,13 +81,13 @@ class CustomSet {
      //Let the garbage collect the old object as we reference the new array instead.
      m_objects = obj_extended_objects;
 
-			}
+   }
 
    //Now that we're sure that we have room, add the object and keep track of the next insertion point.
    m_objects [ m_i_next_index ++ ] = obj;
 
    return true;
-	}
+ }
 
  //Try to find the index of a node.
  //Returns the index of the node on success.
@@ -95,22 +95,22 @@ class CustomSet {
  int findNode ( Object obj ) {
    if ( m_i_next_index == 0 ) {
      return -1;
-			}
+   }
 
    for ( int i = 0; i < m_i_next_index; i ++ ) {
      if ( obj.equals ( m_objects [ i ] ) ) {
        return i;
-					} //if objects are equal.
-			} //for
+     } //if objects are equal.
+   } //for
    return -1;
-	}
+ }
 
  //Checks to see if a node with a given value exists within the set.
  //Returns true, if the node exists;
  //returns false, if the node does not exist.
  Boolean containsNode ( Object obj ) {
    return findNode ( obj ) != -1;
-	}
+ }
 
  //Removes a node from the set by its value, if it exists.
  //Returns true, if the node was found;
@@ -123,7 +123,7 @@ class CustomSet {
    //If the object couldn't be found just return false.
    if ( i_index == -1 ) {
      return false;
-			}
+   }
 
   //Otherwise, move the last node of the list to this position,
    m_objects [ i_index ] = m_objects [ m_i_next_index - 1 ];
@@ -132,21 +132,21 @@ class CustomSet {
    m_objects [ -- m_i_next_index ] = null;
 
    return true;
-	}
+ }
 
  //Returns the integer index where the next node would be inserted.
  //This is equal to the current number of nodes within the list.
  int getNodeCount (  ) {
    //Since the insertion point is always the next index, it will also be equal to the count.
    return m_i_next_index;
-	}
+ }
 
  //Show all nodes contained within the set in stdout.
  void showNodes (  ) {
    for ( int i = 0; i < m_i_next_index; i ++ ) {
      System.out.print ( "This is the current object: \"" + m_objects [ i ] + "\"\n" );
-			}
-	}
+   }
+ }
 
 }
 
@@ -184,7 +184,7 @@ class UnitTest {
       //Add the words, "This", "is", ..., "test" as individual nodes of the set.
       for ( String str : new String[] { "This", "is", "only", "a", "test" } ) {
         set .addNode ( str );
-						}
+      }
       FakeUnitTest .assertEquals (
         6, //5 to force an exception.
         set .getNodeCount (  )
@@ -195,18 +195,18 @@ class UnitTest {
       //Setting i to 155 will trigger the DEBUG: Doubling the size of the array from __ to __ message one last time.
       for ( int i = 0; i < 154; i ++ ) {
         set .addNode ( i );
-						}
+      }
 
       FakeUnitTest .assertEquals (
         160, //The number of nodes in the list should be 160, which means that the highest index is currently 159.
         set .getNodeCount (  )
       );
 
-			 } catch ( Exception e ) {
+    } catch ( Exception e ) {
       System.out.print ( "Here's the error: \"" + e .getMessage (  ) + "\"\n" );
-			 } catch ( Throwable t ) {
+    } catch ( Throwable t ) {
       System.out.print ( "Here's the error: \"" + t .getMessage (  ) + "\"\n" );
-			 }
+    }
 
-	 }
+  }
 }
