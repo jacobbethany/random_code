6bb79263109cf1bee429e6f54af1a5b127961a9b
jacobbethany <jabwareman@gmail.com>
2022-08-14 21:48:48 -0700

    Update json_test.cpp
    I've modified the tests along with the rest of the project. Most notably, the project is safer because I've deleted the copy constructor.
    
    If a function had returned a locally scoped VARIABLE instance, it would have copied the address of some memory used internally, which would have also been freed by the destructor (if not subject to compiler optimization causing elision). This would have caused undefined behavior. With that in mind, I've made another overload for the parse function that takes a locally scoped VARIABLE instance thus preventing the need to manually delete dynamically instantiated VARIABLE objects.
    
    I've also optionally disabled some methods of creating new objects. This can be re-enabled via preprocessor define within JSON.H called JSON_ENFORCE_SAFE_USAGE.

--- json_test.cpp
+++ json_test.cpp
@@ -3,13 +3,25 @@
  Purpose: To move the tests out of the main JSON source file.
  Ask for permission before using.
 
+ Last updated on 2022-08-15.
+ -I made it so that the tests use better practices than the copy constructor,
+ since there is a buffer that is freed, internally during the destructor calls
+ which would still be used by a copy returned from a function.
+ -I've conditionally deleted the default copy constructor and have made a new
+ version of VARIABLE::parse that allows for passing an existing instance of a
+ VARIABLE class, rather than having a dynamically instantiated instance of one
+ be returned. The copy constructor can be reenabled by setting the define,
+ JSON_ENFORCE_SAFE_USAGE, to 0. This is defined in json.h.
+
  To compile:
- g++ json.cpp json_test.cpp -o json
+   g++ json.cpp json_test.cpp -o json
 */
  #include "json.h"
 
  void json_minimize_test ( void )
 {{
+ printf ( "Beginning test (\"json_minimize_test\").\n" );
+
  const char *sz_string = "{ \"test\" : [ 1, 2, 3 ] }";
  std::string minimized_json_string = VARIABLE::get_minimal_json_string ( sz_string );
  printf ( "Minimized json: \"%s\"\n", minimized_json_string .c_str (  ) );
@@ -17,8 +29,16 @@
 
  void test1 ( void )
 {{
+ printf ( "Beginning test #1.\n" );
+
+#if ( ! JSON_ENFORCE_SAFE_USAGE )
  VARIABLE tim_vector = VARIABLE::empty_array (  ); //same as //VARIABLE jacob_vector ( NEW_ARRAY );
  auto jacob_vector = VARIABLE::empty_array (  ); //same as //VARIABLE tim_vector ( std::vector<VARIABLE *>() );
+#else
+ VARIABLE tim_vector ( NEW_ARRAY );
+ VARIABLE jacob_vector ( NEW_ARRAY );
+#endif
+
  jacob_vector .add ( "guitar" );
  jacob_vector .add ( "gayming" );
  jacob_vector .add ( "frolicing in the nude" );
@@ -37,16 +57,26 @@
 
  //printf ( "%s\n", jacob_vector .to_json (  ) .c_str (  ) );
 
+#if ( ! JSON_ENFORCE_SAFE_USAGE )
  VARIABLE main_object = VARIABLE::empty_object (  ); //same as VARIABLE main_object ( NEW_OBJECT ); or VARIABLE main_object ( std::map<std::string, VARIABLE *>() );
+#else
+ VARIABLE main_object ( NEW_OBJECT );
+#endif
  main_object .add ( "Jacob", &jacob_object );
  main_object .add ( "Tim", &tim_object );
  printf ( "%s\n\n", main_object .to_json (  ) .c_str (  ) );
 }}
 
- void test_at (  )
+ void test_at ( void )
 {{
+ printf ( "Beginning test (\"test_at\").\n" );
+
  //Create a new object.
+#if ( ! JSON_ENFORCE_SAFE_USAGE )
  VARIABLE obj = VARIABLE::empty_object (  );
+#else
+ VARIABLE obj ( NEW_OBJECT );
+#endif
  printf ( "A new object was created.\n" );
 
  //Start filling the object.
@@ -60,8 +90,9 @@
  printf ( "VARIABLE::at(10) =>\n%s\n\n", lp_variable ->to_json (  ) .c_str (  ) );
 }}
 
- void test2 (  )
+ void test2 ( void )
 {{
+ printf ( "Beginning test #2.\n" );
 /*
  {
   "Jacob" : {
@@ -78,20 +109,41 @@
   }
  }
 */
-
+#if ( ! JSON_ENFORCE_SAFE_USAGE )
  VARIABLE obj = VARIABLE::empty_object (  );
+#else
+ VARIABLE obj ( NEW_OBJECT );
+#endif
+
+#if ( ! JSON_ENFORCE_SAFE_USAGE )
  obj .add ( "Jacob", VARIABLE::new_object (  ) );
+#else
+ obj .add ( "jacob", new VARIABLE ( NEW_OBJECT ) );
+#endif
  obj .at ( "Jacob" ) ->add ( "Age", (int64_t) 30 );
+#if ( ! JSON_ENFORCE_SAFE_USAGE )
  obj .at ( "Jacob" ) ->add ( "Hobbies", VARIABLE::new_array (  ) );
+#else
+ obj .at ( "Jacob" ) ->add ( "Hobbies", new VARIABLE ( NEW_ARRAY ) );
+#endif
  obj .at ( "Jacob" ) ->at ( "Hobbies" ) ->add ( "Guitar" );
  obj .at ( "Jacob" ) ->at ( "Hobbies" ) ->add ( "Programming" );
  obj .at ( "Jacob" ) ->at ( "Hobbies" ) ->add ( "Gayming" );
  obj .at ( "Jacob" ) ->at ( "Hobbies" ) ->add ( "frolicing in the nude" );
  obj .at ( "Jacob" ) ->at ( "Hobbies" ) ->add ( "\"Creating strings\" that \\need\\ to be \\e\"s\"\\c\\ap\\e\\d'`~" );
 
+#if ( ! JSON_ENFORCE_SAFE_USAGE )
  obj .add ( "Tim", VARIABLE::new_object (  ) );
+#else
+ obj .add ( "Tim", new VARIABLE ( NEW_OBJECT ) );
+#endif
  obj .at ( "Tim" ) ->add ( "Age", (int64_t) 48 );
+
+#if ( ! JSON_ENFORCE_SAFE_USAGE )
  obj .at ( "Tim" ) ->add ( "Hobbies", VARIABLE::new_array (  ) );
+#else
+ obj .at ( "Tim" ) ->add ( "Hobbies", new VARIABLE ( NEW_ARRAY ) );
+#endif
  obj .at ( "Tim" ) ->at ( "Hobbies" ) ->add ( "Complaining" );
  obj .at ( "Tim" ) ->at ( "Hobbies" ) ->add ( "Guitar" );
  obj .at ( "Tim" ) ->at ( "Hobbies" ) ->add ( "Programming" );
@@ -103,6 +155,8 @@
 
  void test3 ( void )
 {{
+ printf ( "Beginning test #3.\n" );
+
  VARIABLE obj;
 /*
  printf ( "New object:\n%s\n", obj .to_json (  ). c_str (  ) );
@@ -161,20 +215,21 @@
  std::string json_string = obj .to_minimal_json (  );
  printf ( "Final object:\n%s\n", json_string. c_str (  ) );
 
- VARIABLE *json_object = VARIABLE::parse ( json_string .c_str (  ) );
+ VARIABLE json_object;
+ VARIABLE::parse ( json_object, json_string .c_str (  ) );
 
- printf ( "Here's the parsed json object:\n%s\n\n", json_object ->to_json (  ) .c_str (  ) );
+ printf ( "Here's the parsed json object:\n%s\n\n", json_object .to_json (  ) .c_str (  ) );
 
  printf ( "The destructor is about to be called.\n" );
- delete json_object;
- printf ( "json_object is now freed, but the local scope of obj will now end and call an identical destructor.\n" );
 }}
 
 /*
  This is a test to ensure that the get_[type] functions are working, properly.
 */
- void test4 (  )
+ void test4 ( void )
 {{
+ printf ( "Beginning test #4.\n" );
+
  VARIABLE v;
  v [ "array" ] .add ( (int64_t) 0 );
  v [ "array" ] .add ( 1.0 );
@@ -241,6 +296,8 @@
 */
  void test5 ( void )
 {{
+ printf ( "Beginning test #5.\n" );
+
  VARIABLE ary;
  ary .add ( (int64_t) 0 );
  ary .add ( 1.0 );
@@ -256,35 +313,101 @@
 */
  void test6 ( void )
 {{
+ printf ( "Beginning test #6.\n" );
+
  VARIABLE integer ( (int64_t) 777 );
  printf ( "%s\n", integer .to_json (  ) .c_str (  ) );
- VARIABLE *json = VARIABLE::parse ( integer .to_json (  ) .c_str (  ) );
- printf ( "%s\n", json ->to_json (  ) .c_str (  ) );
- delete json;
-
+ VARIABLE json;
+ VARIABLE::parse ( json, integer .to_json (  ) .c_str (  ) );
+ printf ( "%s\n", json .to_json (  ) .c_str (  ) );
+// json.clear();
 
  VARIABLE double_variable ( 7.77 );
  printf ( "%s\n", double_variable .to_json (  ) .c_str (  ) );
- json = VARIABLE::parse ( double_variable .to_json (  ) .c_str (  ) );
- printf ( "%s\n", json ->to_json (  ) .c_str (  ) );
- delete json;
-
+ VARIABLE::parse ( json, double_variable .to_json (  ) .c_str (  ) );
+ printf ( "%s\n", json .to_json (  ) .c_str (  ) );
+// json.clear();
 
  VARIABLE string_variable ( "This is only a test." );
  printf ( "%s\n", string_variable .to_json (  ) .c_str (  ) );
- json = VARIABLE::parse ( string_variable .to_json (  ) .c_str (  ) );
- printf ( "%s\n", json ->to_json (  ) .c_str (  ) );
- delete json;
+ VARIABLE::parse ( json, string_variable .to_json (  ) .c_str (  ) );
+ printf ( "%s\n", json .to_json (  ) .c_str (  ) );
 }}
+
+ void test7 ( void ) {
+   printf ( "Beginning test #7.\n" );
+
+   VARIABLE v;
+   v.add ( "test" );
+   v.add ( (int64_t) 1 );
+   v.add ( (double) 1.1 );
+
+   //This does the same as below.
+   VARIABLE v2;
+   v2 [ "key" ] = "value";
+   v2 [ "ary" ] = new VARIABLE ( );
+   v2 [ "ary" ] .add ( "This" );
+   v2 [ "ary" ] .add ( "is" );
+   v2 [ "ary" ] .add ( "a" );
+   v2 [ "ary" ] .add ( "test" );
+
+   v.add ( &v2 );
+
+   printf (
+     "%hs\n",
+     v .to_json (  ) .c_str (  )
+   );
+
+ }
+
+ //Same as test7, but with different approach/syntax.
+ //I also showcased the differences in new VARIABLE
+ //instances between objects and array types.
+ void test8 ( void ) {
+   printf ( "Beginning test #8.\n" );
+
+   VARIABLE v;
+   v.add ( "test" );
+   v.add ( (int64_t) 1 );
+   v.add ( (double) 1.1 );
+
+   //v.add ( new VARIABLE (  ) ); //this would fail.
+   //This NEW_OBJECT is required because
+   //objects that hold invalid types can't
+   //be added to arrays.
+   v.add ( new VARIABLE ( NEW_OBJECT ) );
+
+   //This "will" work but it will add "unsupported type"
+   //as the value. The key is correct.
+   (*v.at(3)) [ "empty_obj" ] = new VARIABLE (  );
+//   (*v.at(3)) [ "empty_obj" ] = (int64_t) 10; //works... (it's now a valid type.)
+
+   (*v.at ( 3 )) [ "key" ] = "value";
+   (*v.at ( 3 )) [ "ary" ] .add ( "This" );
+   v .at ( 3 ) ->at ( "ary" ) ->add ( "is" ); //different syntax, same result.
+   (*v.at ( 3 )) [ "ary" ] .add ( "a" );
+   (*v.at ( 3 )) [ "ary" ] .add ( "test" );
+
+   printf (
+     "%hs\n",
+     v .to_json (  ) .c_str (  )
+   );
+
+ }
 
  int main ( int argc, char **argv )
 {{
- //test1 (  );
- //test2 (  );
+ json_minimize_test (  );
+ test_at (  );
+
+ test1 (  );
+ test2 (  );
  test3 (  );
- //test4 (  );
- //test5 (  );
- //test6 (  );
+ test4 (  );
+ test5 (  );
+ test6 (  );
+ test7 (  );
+ test8 (  );
 
  return 0;
 }}
